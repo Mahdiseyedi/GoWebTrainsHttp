2b528c1d59cf90d784eab0656f5a83478b91468b
Mahdi_seyedi <mahdisayyedi19@gmail.com>
2023-06-20 19:51:26 +0330

    Query Params in Echo

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 )
 
 func getUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusCreated, id+"\n")
+	name := c.QueryParam("username")
+	family := c.QueryParam("family")
+
+	return c.String(http.StatusOK, name+" + "+family+"\n")
 }
 
 func main() {
 	e := echo.New()
 
-	e.GET("/:id", getUser)
+	e.GET("/users", getUser)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
