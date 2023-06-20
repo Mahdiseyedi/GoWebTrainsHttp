e6e87e5461152f8c666d9db3a3cfbef498ae3ac8
Mahdi_seyedi <mahdisayyedi19@gmail.com>
2023-06-20 19:32:26 +0330

    routing in echo

--- 03_02Echo_Routing/Routing.go
+++ 03_02Echo_Routing/Routing.go
@@ -1,28 +1,4 @@
-package mainpackage main
-
-import (
-	"github.com/labstack/echo/v4"
-	"net/http"
-)
-
-func main() {
-	e := echo.New()
-	e.POST("/", func(c echo.Context) error {
-		return c.String(http.StatusCreated, "this is post route")
-	})
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "this is get route")
-	})
-	e.PUT("/", func(c echo.Context) error {
-		return c.String(http.StatusCreated, "this is put route")
-	})
-	e.PATCH("/", func(c echo.Context) error {
-		return c.String(http.StatusCreated, "this is patch route")
-	})
-
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
+package main
 
 import (
 	"github.com/labstack/echo/v4"
